342303d03bb98298012955ba1416b48f1c66081d
root <saung.myat@gmail.com>
2022-02-26 20:56:40 -0500

    commit tree 0fd13cd16e8a009fa6791e21289ad94402dcecd3

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Saung';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toLocaleUpperCase() + '!';
+
+const shortGreeting = 'Welcome, ' + currentUser.slice(0, 1) + '!';
